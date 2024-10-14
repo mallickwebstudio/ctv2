4fe366a7f29a1d7d92c194073668553338664606
webdevsalmann <webdevsalman7869@gmail.com>
2024-10-14 20:14:42 +0530

    Minor fix

--- src/lib/datas/api.js
+++ src/lib/datas/api.js
@@ -1,4 +1,4 @@
-export const baseUrl = process.env.NEXT_PUBLIC_HOST === "dev" ? "" : "http://52.215.93.118";
+export const baseUrl = process.env.NEXT_PUBLIC_HOST === "dev" ? "http://52.215.93.118" : "";
 
 // Get Country
 // http://52.215.93.118/api/get-country
